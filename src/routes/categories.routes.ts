9214fca782361a2f51066457e261f7cf5212cf94
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-06-28 11:48:40 -0300

    feat: import service create category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,6 @@
 import { Response, Request, Router } from "express";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router()
 const categoriesRepository = new CategoriesRepository()
@@ -7,13 +8,9 @@ const categoriesRepository = new CategoriesRepository()
 categoriesRoutes.post("/", (req: Request, res: Response) => {
     const { name, type, platforms, description } = req.body
 
-    const categoryAlreadyExists = categoriesRepository.findByName(name)
+    const createCategoryService = new CreateCategoryService(categoriesRepository)
 
-    if (categoryAlreadyExists) {
-        return res.status(400).json({error: "Category already exists"})
-    }
-
-    categoriesRepository.create({ name, type, platforms, description })
+    createCategoryService.execute({ name, type, platforms, description })
 
     return res.status(201).send()
 })
