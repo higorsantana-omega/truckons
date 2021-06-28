e57bdc4622016f77a56290d92be1899d8b60470b
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-06-28 10:39:56 -0300

    feat: validate category already exists

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -9,6 +9,12 @@ const categoriesRepository = new CategoriesRepository()
 categoriesRoutes.post("/", (req: Request, res: Response) => {
     const { name, type, platforms, description } = req.body
 
+    const categoryAlreadyExists = categoriesRepository.findByName(name)
+
+    if (categoryAlreadyExists) {
+        return res.status(400).json({error: "Category already exists"})
+    }
+
     categoriesRepository.create({ name, type, platforms, description })
 
     return res.status(201).send()
