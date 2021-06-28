0c1001d5c82296895ad2e00f9ad1dba4f00b662b
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-06-28 00:49:54 -0300

    create routes categories

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,21 @@
+import { Request, Router } from "express";
+import { Response } from "express-serve-static-core";
+
+const categoriesRoutes = Router()
+
+const categories = []
+
+categoriesRoutes.post("/categories", (req: Request, res: Response) => {
+    const { name, type, platforms, description } = req.body
+
+    categories.push({
+        name,
+        type,
+        platforms,
+        description
+    })
+
+    return res.status(201)
+})
+
+export { categoriesRoutes }
