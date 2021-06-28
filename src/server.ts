51c8924ae49adf50a4fa2bca42a468d1a4b04d58
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-06-28 00:50:50 -0300

    refactor server

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,9 @@
 import express from 'express'
-import { createGiftCard } from './routes'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello' })
-})
+app.use(express.json())
 
-app.get('/s', createGiftCard)
 
-function test() {
-  return {a: 1}
-}
 
 app.listen(3098, () => console.log("Rodando servidor!"))
