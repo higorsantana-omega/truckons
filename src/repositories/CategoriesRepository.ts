be26ef9ead2efbd6bf02d67a9606ab0d995139c3
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-06-28 10:32:42 -0300

    feat: function list categories

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -29,6 +29,10 @@ class CategoriesRepository {
 
         this.categories.push(category)
     }
+
+    list(): Category[] {
+        return this.categories
+    }
 }
 
 export { CategoriesRepository }
