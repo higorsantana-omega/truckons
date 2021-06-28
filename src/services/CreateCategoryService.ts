ea3600b8e33ef592ebd8abaf0dcf00fc0acf4e6e
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-06-28 11:48:22 -0300

    feat: create category

--- src/services/CreateCategoryService.ts
+++ src/services/CreateCategoryService.ts
@@ -1,7 +1,26 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepository"
 
+interface IRequest{
+    name: string
+    type: string
+    platforms: string
+    description: string
+}
 
 class CreateCategoryService{
+    constructor(private categoriesRepo: CategoriesRepository) {
+        
+    }
 
+    execute({ name, type, platforms, description }: IRequest): void {
+        const categoryAlreadyExists = this.categoriesRepo.findByName(name)
+
+        if (categoryAlreadyExists) {
+            throw new Error("Category already exists")
+        }
+
+        this.categoriesRepo.create({ name, type, platforms, description })
+    }
 }
 
 export { CreateCategoryService }
