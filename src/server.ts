0fcf73f01cc194525feadf81c06f1bfa2886382a
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-06-28 00:51:29 -0300

    add: routes categories

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express'
+import { categoriesRoutes } from './routes/categories.routes'
 
 const app = express()
 
 app.use(express.json())
 
-
+app.use(categoriesRoutes)
 
 app.listen(3098, () => console.log("Rodando servidor!"))
