5cbf3f34f63ec90ad708a4f703f3a45b4c66be11
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-06-29 11:00:42 -0300

    refactor: singleton

--- src/modules/gifts/repositories/CategoriesRepository.ts
+++ src/modules/gifts/repositories/CategoriesRepository.ts
@@ -5,9 +5,17 @@ import { ICategoriesRepository, ICreateCategory } from "./ICategoriesRepository"
 class CategoriesRepository implements ICategoriesRepository {
     private categories: Category[]
 
+    private static INSTANCE: CategoriesRepository
     
-    constructor() {
+    private constructor() {
         this.categories = []
+    }
+
+    public static getInstance(): CategoriesRepository{
+        if (!CategoriesRepository.INSTANCE) {
+            CategoriesRepository.INSTANCE = new CategoriesRepository()
+        }
+        return CategoriesRepository.INSTANCE
     }
 
     create({ name, type, platforms, description } : ICreateCategory): void {
