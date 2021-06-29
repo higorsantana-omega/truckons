e23958ba7f10dedd101b39ba6eba396d80e3d07b
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-06-29 13:53:35 -0300

    feat: route import

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,11 @@
 import { Response, Request, Router } from "express";
 import { createCategoryController } from "../modules/gifts/useCases/createCategory";
 import { listCategoriesController } from "../modules/gifts/useCases/listCategories";
+import multer from "multer";
 
+const upload = multer({
+    dest: "./tmp"
+})
 const categoriesRoutes = Router()
 
 categoriesRoutes.post("/", (req: Request, res: Response) => {
@@ -10,6 +14,12 @@ categoriesRoutes.post("/", (req: Request, res: Response) => {
 
 categoriesRoutes.get("/", (req: Request, res: Response) => {
     return listCategoriesController.handle(req, res)
+})
+
+categoriesRoutes.post("/import",  upload.single("file"),(req, res) => {
+    const { file } = req
+    console.log(file)
+    return res.send()
 })
 
 export { categoriesRoutes }
