0a11968b23b57627681b2df730f335d47a74600f
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-07-04 22:08:03 -0300

    feat: create route specification

---
+++ src/routes/specification.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express"
+import { SpecificationsRepository } from "../modules/trucks/repositories/implementations/SpecificationsRepository"
+import { CreateSpecificationService } from "../modules/trucks/services/CreateSpecificationService"
+
+const specificationsRoutes = Router()
+const specificationsRepository = new SpecificationsRepository()
+
+
+specificationsRoutes.post("/", (req, res) => {
+    const { name, description } = req.body
+    const createSpecificationService = new CreateSpecificationService(specificationsRepository)
+    createSpecificationService.execute({ name, description })
+    return res.status(201).send()
+})
+
+export { specificationsRoutes }
