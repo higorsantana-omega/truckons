eec00281bd2da22b987cbc8fe8e4b5611afb2b58
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-07-05 11:05:39 -0300

    create first migration

---
+++ src/database/migrations/1625493912269-CreateCategories.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateCategories1625493912269 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
