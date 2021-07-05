db3ad5659e5abe2d7d4b40d62c181e5afe7a8368
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-07-05 11:42:37 -0300

    constructor public

--- src/modules/trucks/repositories/implementations/CategoriesRepository.ts
+++ src/modules/trucks/repositories/implementations/CategoriesRepository.ts
@@ -8,15 +8,8 @@ class CategoriesRepository implements ICategoriesRepository {
 
     private static INSTANCE: CategoriesRepository
     
-    private constructor() {
+    constructor() {
         this.repository = getRepository(Category)
-    }
-
-    public static getInstance(): CategoriesRepository{
-        if (!CategoriesRepository.INSTANCE) {
-            CategoriesRepository.INSTANCE = new CategoriesRepository()
-        }
-        return CategoriesRepository.INSTANCE
     }
 
     async create({ name, description } : ICreateCategory): Promise<void> {
