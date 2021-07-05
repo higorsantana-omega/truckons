00c72a92686d6db8b36763bd2b34d6bd42b047d0
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-07-05 10:43:06 -0300

    rename imports

--- src/routes/specification.routes.ts
+++ src/routes/specification.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express"
 import { SpecificationsRepository } from "../modules/trucks/repositories/implementations/SpecificationsRepository"
-import { CreateSpecificationService } from "../modules/trucks/services/CreateSpecificationService"
+import { CreateSpecificationUseCase } from "../modules/trucks/useCases/createSpecification/CreateSpecificationUseCase"
 
 const specificationsRoutes = Router()
 const specificationsRepository = new SpecificationsRepository()
@@ -8,7 +8,7 @@ const specificationsRepository = new SpecificationsRepository()
 
 specificationsRoutes.post("/", (req, res) => {
     const { name, description } = req.body
-    const createSpecificationService = new CreateSpecificationService(specificationsRepository)
+    const createSpecificationService = new CreateSpecificationUseCase(specificationsRepository)
     createSpecificationService.execute({ name, description })
     return res.status(201).send()
 })
