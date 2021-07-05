cb00d3b5a92ccb6f3ecc987e19662cd1111be59f
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-07-04 22:01:13 -0300

    feat: new findbyname

--- src/modules/trucks/services/CreateSpecificationService.ts
+++ src/modules/trucks/services/CreateSpecificationService.ts
@@ -1,6 +1,19 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationRepository";
+
+interface IRequest {
+    name: string
+    description: string
+}
+
 class CreateSpecificationService{
-    execute() {
+    constructor(private specificationsRepository: ISpecificationsRepository) {
         
+    }
+    execute({name, description}: IRequest): void {
+        this.specificationsRepository.create({
+            name,
+            description,
+        })
     }
 }
 
