8f0c7621e3f75f320af0750a476013f1ff124910
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-07-06 11:29:42 -0300

    feat: refactoring routes after dependencyinjection

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,18 @@
 import { Response, Request, Router } from "express";
-import createCategoryController from "../modules/trucks/useCases/createCategory";
+import { CreateCategoryController } from "../modules/trucks/useCases/createCategory/CreateCategoryController";
 import { listCategoriesController } from "../modules/trucks/useCases/listCategories";
 import multer from "multer";
 import { importCategoryController } from "../modules/trucks/useCases/importCategory";
 
+const categoriesRoutes = Router()
+
 const upload = multer({
     dest: "./tmp"
 })
-const categoriesRoutes = Router()
 
-categoriesRoutes.post("/", (req: Request, res: Response) => {
-   return createCategoryController().handle(req, res)
-})
+const createCategoryController = new CreateCategoryController()
+
+categoriesRoutes.post("/", createCategoryController.handle)
 
 categoriesRoutes.get("/", (req: Request, res: Response) => {
     return listCategoriesController.handle(req, res)
