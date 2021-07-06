ea611f4add858381a0467d1409a475909710897c
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-07-06 12:11:34 -0300

    feat: injectable in SpecificationUseCase

--- src/modules/trucks/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/trucks/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,3 +1,4 @@
+import { inject, injectable } from "tsyringe";
 import { ISpecificationsRepository } from "../../repositories/ISpecificationRepository";
 
 interface IRequest {
@@ -5,10 +6,12 @@ interface IRequest {
     description: string
 }
 
+@injectable()
 class CreateSpecificationUseCase{
-    constructor(private specificationsRepository: ISpecificationsRepository) {
-        
-    }
+    constructor(
+        @inject("SpecificationsRepository")
+        private specificationsRepository: ISpecificationsRepository) { }
+    
     execute({ name, description }: IRequest): void {
         const specificationAlreadyExists = this.specificationsRepository.findByName(name)
         if (specificationAlreadyExists) {
