98864b3586915d2be59d84ad77b206aae5f28432
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-07-06 12:10:16 -0300

    feat: dependency injection Specificatio Repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,15 @@
 import { container } from "tsyringe"
 import { ICategoriesRepository } from "../../modules/trucks/repositories/ICategoriesRepository"
 import { CategoriesRepository } from "../../modules/trucks/repositories/implementations/CategoriesRepository"
+import { SpecificationsRepository } from "../../modules/trucks/repositories/implementations/SpecificationsRepository"
+import { ISpecificationsRepository } from "../../modules/trucks/repositories/ISpecificationRepository"
 
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository",
     CategoriesRepository
+)
+
+container.registerSingleton<ISpecificationsRepository>(
+    "SpecificationsRepository",
+    SpecificationsRepository
 )
