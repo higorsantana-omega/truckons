91957a887c182dec97103dc843444f1f643c7c63
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-07-07 14:50:43 -0300

    verify email already exists

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,7 +1,9 @@
 import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
+import { User } from "../entities/User";
 
 interface IUSersRepository {
     create(data: ICreateUserDTO): Promise<void>
+    findByEmail(email: string): Promise<User>
 }
 
 export { IUSersRepository }
--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcrypt";
 import { inject, injectable } from "tsyringe";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUSersRepository } from "../../repositories/IUsersRepository";
@@ -10,11 +11,20 @@ class CreateUserUseCase {
     ) {}
 
     async execute({ name, email, driver_license, password }: ICreateUserDTO): Promise<void> {
+
+        const emailAlreadyExists = await this.usersRepository.findByEmail(email)
+
+        if (emailAlreadyExists) {
+            throw new Error("Email already exists");
+        }
+
+        const passwordHash = await hash(password, 9)
+        
         await this.usersRepository.create({
             name,
             email,
             driver_license,
-            password
+            password: passwordHash
         })
     }
 
