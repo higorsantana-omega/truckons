1f6a34edeeae6fecb360ddee31ddf5a01aabd841
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-07-14 12:52:02 -0300

    feat: indent size

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -4,34 +4,34 @@ import { AppError } from "../errors/AppError";
 import { UsersRepository } from "../modules/accounts/repositories/implementations/UserRepository";
 
 interface IPayLoad {
-    sub: string
+  sub: string
 }
 
 export async function ensureAuthenticated(req: Request, res: Response, next: NextFunction) {
-    const authHeader = req.headers.authorization
+  const authHeader = req.headers.authorization
 
-    if (!authHeader) {
-        throw new AppError("Token missing", 401);
+  if (!authHeader) {
+    throw new AppError("Token missing", 401);
+  }
+
+  const [, token] = authHeader.split(" ")
+
+  try {
+    const { sub: user_id } = verify(token, "7b96e636e4bd247fc6dfe3371a194766") as IPayLoad
+    const usersRepository = new UsersRepository()
+    const user = usersRepository.findById(user_id)
+
+    if (!user) {
+      throw new AppError("User does not exists", 401);
     }
 
-    const [, token] = authHeader.split(" ")
-
-    try {
-        const { sub: user_id } = verify(token, "7b96e636e4bd247fc6dfe3371a194766") as IPayLoad
-        const usersRepository = new UsersRepository()
-        const user = usersRepository.findById(user_id)
-
-        if (!user) {
-            throw new AppError("User does not exists", 401);
-        }
-
-        req.user = {
-            id: user_id
-        }
-
-        next()
-    } catch (error) {
-        throw new AppError("Invalid token", 401);
+    req.user = {
+      id: user_id
     }
+
+    next()
+  } catch (error) {
+    throw new AppError("Invalid token", 401);
+  }
 
 }
