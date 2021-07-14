20c7f6a203b1b797d6e767710a1ab6f90dc83f4f
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-07-14 14:34:49 -0300

    feat: refactor modules

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,6 +1,6 @@
-import { AppError } from "../../../../errors/AppError"
-import { ICreateUserDTO } from "../../dtos/ICreateUserDTO"
-import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory"
+import { AppError } from "@errors/AppError"
+import { ICreateUserDTO } from "@modules/accounts/dtos/ICreateUserDTO"
+import { UsersRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersRepositoryInMemory"
 import { CreateUserUseCase } from "../createUser/CreateUserUseCase"
 import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase"
 
@@ -9,53 +9,53 @@ let usersRepositoryInMemory: UsersRepositoryInMemory
 let createUserUseCase: CreateUserUseCase
 
 describe("Authenticate User", () => {
-    beforeEach(() => {
-        usersRepositoryInMemory = new UsersRepositoryInMemory()
-        authenticateUserUseCase = new AuthenticateUserUseCase(usersRepositoryInMemory)
-        createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory)
+  beforeEach(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory()
+    authenticateUserUseCase = new AuthenticateUserUseCase(usersRepositoryInMemory)
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory)
+  })
+
+  it("should be able to authenticate an user", async () => {
+    const user: ICreateUserDTO = {
+      driver_license: "0012221",
+      email: "user@test.com",
+      password: "123",
+      name: "User_Test"
+    }
+    await createUserUseCase.execute(user)
+
+    const result = await authenticateUserUseCase.execute({
+      email: user.email,
+      password: user.password
     })
 
-    it("should be able to authenticate an user", async () => {
-        const user: ICreateUserDTO = {
-            driver_license: "0012221",
-            email: "user@test.com",
-            password: "123",
-            name: "User_Test"
-        }
-        await createUserUseCase.execute(user)
+    expect(result).toHaveProperty("token")
+  })
 
-        const result = await authenticateUserUseCase.execute({
-            email: user.email,
-            password: user.password
-        })
+  it("should not be able to authenticate an no existent user", () => {
+    expect(async () => {
+      await authenticateUserUseCase.execute({
+        email: "user@test.com",
+        password: "123"
+      })
+    }).rejects.toBeInstanceOf(AppError)
+  })
 
-        expect(result).toHaveProperty("token")
-    })
+  it("should not be able to authenticate with incorrect password", () => {
+    expect(async () => {
+      const user: ICreateUserDTO = {
+        driver_license: "9999",
+        email: "user@email.com",
+        password: "0228121",
+        name: "user Error"
+      }
 
-    it("should not be able to authenticate an no existent user", () => {
-        expect(async () => {
-            await authenticateUserUseCase.execute({
-                email: "user@test.com",
-                password: "123"
-            })
-        }).rejects.toBeInstanceOf(AppError)
-    })
+      await createUserUseCase.execute(user)
 
-    it("should not be able to authenticate with incorrect password", () => {
-        expect(async () => {
-            const user: ICreateUserDTO = {
-                driver_license: "9999",
-                email: "user@email.com",
-                password: "0228121",
-                name: "user Error"
-            }
-
-            await createUserUseCase.execute(user)
-
-            await authenticateUserUseCase.execute({
-                email: user.email,
-                password: "passwordincorrect"
-            })
-        }).rejects.toBeInstanceOf(AppError)
-    })
+      await authenticateUserUseCase.execute({
+        email: user.email,
+        password: "passwordincorrect"
+      })
+    }).rejects.toBeInstanceOf(AppError)
+  })
 })
