8e90646965947a6664586df995f3937412f37bec
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-07-15 19:13:34 -0300

    fix: bug imports

--- src/modules/trucks/infra/typeorm/repositories/SpecificationsRepository.ts
+++ src/modules/trucks/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -1,6 +1,6 @@
 import { getRepository, Repository } from "typeorm";
-import { Specification } from "../../entities/Specification";
-import { ICreateSpecificationDTO, ISpecificationsRepository } from "../ISpecificationRepository";
+import { Specification } from "../entities/Specification";
+import { ICreateSpecificationDTO, ISpecificationsRepository } from "@modules/trucks/repositories/ISpecificationRepository"
 
 
 class SpecificationsRepository implements ISpecificationsRepository{
