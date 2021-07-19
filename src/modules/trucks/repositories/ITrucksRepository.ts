0a9aa28d2f7bfbf22bcad183f99a0f5c5083334e
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-07-19 14:39:25 -0300

    feat: create findby

--- src/modules/trucks/repositories/ITrucksRepository.ts
+++ src/modules/trucks/repositories/ITrucksRepository.ts
@@ -1,7 +1,9 @@
 import { ICreateTruckDTO } from "../dtos/ICreateTruckDTO";
+import { Truck } from "../infra/typeorm/entities/Truck";
 
 interface ITrucksRepository {
-  create(data: ICreateTruckDTO): Promise<void>
+  create(data: ICreateTruckDTO): Promise<Truck>
+  findByLicensePlate(license_plate: string): Promise<Truck>
 }
 
 export { ITrucksRepository }
