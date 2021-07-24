9b0c288b37e921ebc194d4b0b792f386a6dddaba
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-07-24 17:06:15 -0300

    feat: rename

--- src/modules/trucks/useCases/listAvailableTrucks/ListAvailableTrucksUseCase.spec.ts
+++ src/modules/trucks/useCases/listAvailableTrucks/ListAvailableTrucksUseCase.spec.ts
@@ -1,14 +1,14 @@
-import { TrucksRepositoryInMemory } from "@modules/trucks/repositories/in-memory/TrucksRepositoryInMemory"
-import { ListTrucksUseCase } from "./ListTrucksUseCase"
+import { TrucksRepositoryInMemory } from "@modules/trucks/repositories/in-memory/TrucksRepositoryInMemory";
+import { ListTrucksUseCase } from "./ListAvailableTrucksUseCase";
 
-let listTrucksUseCase: ListTrucksUseCase
-let trucksRepositoryInMemory: TrucksRepositoryInMemory
+let listTrucksUseCase: ListTrucksUseCase;
+let trucksRepositoryInMemory: TrucksRepositoryInMemory;
 
 describe("List Trucks", () => {
   beforeEach(() => {
-    trucksRepositoryInMemory = new TrucksRepositoryInMemory()
-    listTrucksUseCase = new ListTrucksUseCase(trucksRepositoryInMemory)
-  })
+    trucksRepositoryInMemory = new TrucksRepositoryInMemory();
+    listTrucksUseCase = new ListTrucksUseCase(trucksRepositoryInMemory);
+  });
   it("should be able to list all available trucks", async () => {
     const truck = await trucksRepositoryInMemory.create({
       name: "Truck1",
@@ -17,13 +17,13 @@ describe("List Trucks", () => {
       license_plate: "7UDJA",
       fine_amount: 150,
       brand: "Truck_brand",
-      category_id: "category_id"
-    })
+      category_id: "category_id",
+    });
 
-    const trucks = await listTrucksUseCase.execute({})
+    const trucks = await listTrucksUseCase.execute({});
 
-    expect(trucks).toEqual([truck])
-  })
+    expect(trucks).toEqual([truck]);
+  });
 
   it("should be able to list all available trucks by brand", async () => {
     const truck = await trucksRepositoryInMemory.create({
@@ -33,15 +33,15 @@ describe("List Trucks", () => {
       license_plate: "7KDJA",
       fine_amount: 150,
       brand: "Truck_brand_test",
-      category_id: "category_id"
-    })
+      category_id: "category_id",
+    });
 
     const trucks = await listTrucksUseCase.execute({
       brand: "Truck_brand_test",
-    })
+    });
 
-    expect(trucks).toEqual([truck])
-  })
+    expect(trucks).toEqual([truck]);
+  });
 
   it("should be able to list all available trucks by name", async () => {
     const truck = await trucksRepositoryInMemory.create({
@@ -51,15 +51,15 @@ describe("List Trucks", () => {
       license_plate: "7KDJA",
       fine_amount: 150,
       brand: "Truck_brand_test",
-      category_id: "category_id"
-    })
+      category_id: "category_id",
+    });
 
     const trucks = await listTrucksUseCase.execute({
       name: "Truck3",
-    })
+    });
 
-    expect(trucks).toEqual([truck])
-  })
+    expect(trucks).toEqual([truck]);
+  });
 
   it("should be able to list all available trucks by category", async () => {
     const truck = await trucksRepositoryInMemory.create({
@@ -69,15 +69,13 @@ describe("List Trucks", () => {
       license_plate: "7KDJA",
       fine_amount: 150,
       brand: "Truck_brand_test",
-      category_id: "1234"
-    })
+      category_id: "1234",
+    });
 
     const trucks = await listTrucksUseCase.execute({
       category_id: "1234",
-    })
+    });
 
-    expect(trucks).toEqual([truck])
-  })
-
-  
-})
+    expect(trucks).toEqual([truck]);
+  });
+});
