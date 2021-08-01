a14b4acf0be69a735203e2da06f6930be77d85e7
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-08-01 14:31:34 -0300

    feat: create migration rentals

---
+++ src/shared/infra/typeorm/migrations/1627838228298-CreateRentals.ts
@@ -0,0 +1,66 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateRentals1627838228298 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "rentals",
+        columns: [
+          { name: "id", type: "uuid", isPrimary: true },
+          {
+            name: "truck_id",
+            type: "uuid",
+          },
+          {
+            name: "user_id",
+            type: "varchar",
+          },
+          {
+            name: "start_date",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "end_date",
+            type: "timestamp",
+          },
+          {
+            name: "expected_return_date",
+            type: "timestamp",
+          },
+          {
+            name: "total",
+            type: "numeric",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FKTruckRental",
+            referencedTableName: "trucks",
+            referencedColumnNames: ["id"],
+            columnNames: ["truck_id"],
+            onDelete: "SET NULL",
+            onUpdate: "SET NULL",
+          },
+          {
+            name: "FKUserRental",
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+            columnNames: ["user_id"],
+            onDelete: "SET NULL",
+            onUpdate: "SET NULL",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("rentals");
+  }
+}
