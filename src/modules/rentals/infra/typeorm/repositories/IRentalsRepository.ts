a1eb4ba98c4e80025e643963ba41ce2454cd2d0f
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-08-01 16:37:47 -0300

    feat: add create

--- src/modules/rentals/infra/typeorm/repositories/IRentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/IRentalsRepository.ts
@@ -1,8 +1,10 @@
+import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
 import { Rental } from "../entities/Rental";
 
 interface IRentalsRepository {
   findOpenRentalByTruck(truck_id: string): Promise<Rental>;
   findOpenRentalByUser(user_id: string): Promise<Rental>;
+  create(data: ICreateRentalDTO): Promise<Rental>
 }
 
 export { IRentalsRepository };
