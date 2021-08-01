dc570788a5ef85e40a270a2a9178a5094557a4a4
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-08-01 14:07:33 -0300

    refactor: format document

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,34 +1,34 @@
-import express, { NextFunction, Request, Response } from 'express'
-import "express-async-errors"
-import "reflect-metadata"
-import swaggerUi from 'swagger-ui-express'
-import swaggerFile from "../../../swagger.json"
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import "reflect-metadata";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "../../../swagger.json";
 
-import createConnection from "@shared/infra/typeorm"
-import "@shared/container"
-import { router } from '../../../shared/infra/http/routes'
-import { AppError } from "../../../shared/errors/AppError"
+import createConnection from "@shared/infra/typeorm";
+import "@shared/container";
+import { router } from "../../../shared/infra/http/routes";
+import { AppError } from "../../../shared/errors/AppError";
 
-createConnection()
-const app = express()
+createConnection();
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-app.use(router)
+app.use(router);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-        return res.status(err.statusCode).json({
-            message: err.message
-        })
-    }
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
 
-    return res.status(500).json({
-        status: "error",
-        message: `Internal server error - ${err.message}`
-    })
-})
+  return res.status(500).json({
+    status: "error",
+    message: `Internal server error - ${err.message}`,
+  });
+});
 
-app.listen(3098, () => console.log("Rodando servidor!"))
+app.listen(3098, () => console.log("Rodando servidor!"));
