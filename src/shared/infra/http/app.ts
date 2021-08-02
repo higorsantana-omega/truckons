1452f01d7d5cfb00eddebbdaedee176b9f47bb37
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-08-02 15:29:48 -0300

    feat: create database for test

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,34 @@
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import "reflect-metadata";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "../../../swagger.json";
+
+import createConnection from "@shared/infra/typeorm";
+import "@shared/container";
+import { router } from "./routes";
+import { AppError } from "../../errors/AppError";
+
+createConnection();
+const app = express();
+
+app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: `Internal server error - ${err.message}`,
+  });
+});
+
+export { app }
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,9 +1,9 @@
-import { Connection, createConnection, getConnectionOptions } from "typeorm"
-import { User } from "@modules/accounts/infra/typeorm/entities/User"
-import { Category } from "@modules/trucks/infra/typeorm/entities/Category" 
-import { Specification } from "@modules/trucks/infra/typeorm/entities/Specification"
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
+import { User } from "@modules/accounts/infra/typeorm/entities/User";
+import { Category } from "@modules/trucks/infra/typeorm/entities/Category";
+import { Specification } from "@modules/trucks/infra/typeorm/entities/Specification";
 
-console.log("Arquivo database")
+console.log("Arquivo database");
 // createConnection({
 //     "type": "postgres",
 //     "host": "localhost",
@@ -15,17 +15,21 @@ console.log("Arquivo database")
 //     logging: false,
 //     "migrations": ["./src/shared/infra/typeorm/migrations/*.ts"],
 //     "entities": ["./src/modules/**/entities/*.ts"],
-  
+
 // }).then(connection => {
 
 // }).catch(error => console.log('Information of error: ', error))
 
 export default async (): Promise<Connection> => {
-  const defaultOptions = await getConnectionOptions()
+  const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: "localhost"
+      host: process.env.NODE_ENV === "test" ? "localhost" : "localhost",
+      database:
+        process.env.NODE_ENV === "test"
+          ? "truckon_test"
+          : defaultOptions.database,
     })
-  )
-}
+  );
+};
